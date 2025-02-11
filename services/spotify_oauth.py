b3bbaecf11abbf05f0d5e4ec71ab8ae37ee40d71
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2025-02-11 08:21:23 +0000

    spotify new finito

--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "9214bb00011c4ac88e9abb2b6ce04ae1"
 SPOTIFY_CLIENT_SECRET = "9412f99b5b05479ba88d0e6fa06d3afd"
-SPOTIFY_REDIRECT_URI = "https://5000-iginiomassa-progettinos-e6gis2lgml2.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-iginiomassa-progettinos-06ic5e6lh2t.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 #config SpotifyOAuth per l'autenticazione e redirect uri
 sp_oauth = SpotifyOAuth(
